d5926ab642fbd20da3de143fead79cf61076edd5
Nicolas Crumrine <crumrinecoding@gmail.com>
2024-02-03 22:34:00 -0600

    jiosdfjksdf

--- data.js
+++ data.js
@@ -1,5 +1,8 @@
+import { fetchWeatherApi } from "openmeteo";
+const url = "https://api.open-meteo.com/v1/forecast";
+
 var app = angular.module("weatherApp", []);
-app.controller("weatherController", function ($scope) {
+app.controller("weatherController", async function ($scope) {
   // Convert the summary of the day to an icon
   $scope.dayIcons = [
     {
@@ -184,6 +187,52 @@ app.controller("weatherController", function ($scope) {
   $scope.Time;
   $scope.Location = "Search. Be specific. API is not perfect.";
   var interval;
+
+  const params = {
+    latitude: 52.52,
+    longitude: 13.41,
+    hourly: "temperature_2m",
+  };
+  const url = "https://api.open-meteo.com/v1/forecast";
+  const responses = await fetchWeatherApi(url, params);
+
+  // Helper function to form time ranges
+  const range = (start, stop, step) =>
+    Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+  // Process first location. Add a for-loop for multiple locations or weather models
+  const response = responses[0];
+
+  // Attributes for timezone and location
+  const utcOffsetSeconds = response.utcOffsetSeconds();
+  const timezone = response.timezone();
+  const timezoneAbbreviation = response.timezoneAbbreviation();
+  const latitude = response.latitude();
+  const longitude = response.longitude();
+
+  const hourly = response.hourly();
+
+  // Note: The order of weather variables in the URL query and the indices below need to match!
+  const weatherData = {
+    hourly: {
+      time: range(
+        Number(hourly.time()),
+        Number(hourly.timeEnd()),
+        hourly.interval()
+      ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
+      temperature2m: hourly.variables(0).valuesArray(),
+    },
+  };
+
+  // `weatherData` now contains a simple structure with arrays for datetime and weather data
+  for (let i = 0; i < weatherData.hourly.time.length; i++) {
+    console.log(
+      weatherData.hourly.time[i].toISOString(),
+      weatherData.hourly.temperature2m[i]
+    );
+  }
+  /*
+
   $scope.callByIP = function (position) {
     document.getElementById("search-bar").value = "";
     $(document).ready(function () {
@@ -328,6 +377,8 @@ app.controller("weatherController", function ($scope) {
       }
     });
   };
+
+  */
 
   $scope.getForecastData = function (weatherInfo, country, region, offset) {
     $scope.currentWeather = $scope.getIcon(weatherInfo.currently.icon);
